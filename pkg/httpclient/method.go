996ede106563a4e4cb6d2e81e6dc39ca6a4b0fdc
chanprogo <2483777043@qq.com>
2021-05-16 20:32:33 +0800

    .

--- pkg/httpclient/method.go
+++ pkg/httpclient/method.go
@@ -6,16 +6,8 @@ import (
 	"net/http"
 )
 
-func PostJson(baseURL string, url string, data []byte, setter ...func(*http.Request)) (body []byte, header http.Header, statusCode int) {
-	return reqJson(baseURL, "POST", url, data, setter...)
-}
-
 func PostByteJson(baseURL string, url string, data []byte, setter ...func(*http.Request)) (body []byte, header http.Header, statusCode int) {
 	return reqJsonData(baseURL, "POST", url, data, setter...)
-}
-
-func Get(baseURL string, url string, data []byte, setter ...func(*http.Request)) (body []byte, header http.Header, statusCode int) {
-	return reqJson(baseURL, "GET", url, data, setter...)
 }
 
 func reqJson(baseURL string, method string, url string, data []byte, setters ...func(*http.Request)) (body []byte, header http.Header, statusCode int) {
